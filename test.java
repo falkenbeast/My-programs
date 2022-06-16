cd93d57e59a51f566d82e5345fb6e00a5d1eacce
falkenbeast <arshayush21@gmail.com>
2022-06-16 12:34:47 +0530

    change

--- test.java
+++ test.java
@@ -1,19 +1,9 @@
-class test
+class test 
 {
-    public static void jug(String a)
-    {
-        int b = a.length();
-        //System.out.println(b);
-        //System.out.println(a);
-        if (b<=1)
-        {
-                return;
-        }
-        System.out.print(a.charAt(a.length()-1));
-         jug(a.substring(0, a.length()-1));
-    }
     public static void main(String []args)
     {
-        jug("ayush srivastava");
+        int arr[][] = {{3,4},{4,3}};
+        int n =arr.length;
+        System.out.println(n);
     }
 }
