6f9c806375a5edd8bcab04c5e527d2e1d410ea3a
falkenbeast <arshayush21@gmail.com>
2022-07-13 19:51:01 +0530

    change

--- progprac/thisandsuper.java
+++ progprac/thisandsuper.java
@@ -3,18 +3,24 @@ class base1
 {
     int ab = 10;
 }
+class base3
+{
+    int ab = 40;
+}
 class base2 extends base1
 {
     int ab = 20;
     void print(int ab)
     {
+        base3 b = new base3();
         
-        System.out.println(ab);
-        System.out.println(this.ab);
-         System.out.println(super.ab);
+        System.out.println(ab);           //takes the variable of current method  
+        System.out.println(this.ab);        //takes the variable of current class
+         System.out.println(super.ab);       //takes the variable of first class
+         System.out.println(b.ab);            //takes the variable of the specific class 
     }
 }
-class thisandsuper
+public class thisandsuper
 {
     public static void main(String [] args)
     {
